f01e190f1a724830afeac86c88b8f0bfd8d1677f
Giulianayarleque <giuliana.yarlequech@gmail.com>
2024-08-03 00:25:34 -0500

    Clase 1

---
+++ Basic/00-hellworld.js
@@ -0,0 +1,23 @@
+//Esto es un comentgario
+
+/*Esto es
+un comentgario
+en varias líneas
+*/
+
+
+console.log("¡Hola, JavaScript!")
+console.log('¡Hola, JavaScript!')
+console.log('¡Hola, JavaScript!')
+
+
+// console.log('¡Hola, JavaScript!')
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
